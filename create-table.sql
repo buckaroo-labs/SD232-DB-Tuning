a87ed1150b0e0851efbd4f301a9070a765d4cffd
buckaroo-labs <46908086+buckaroo-labs@users.noreply.github.com>
2025-06-02 13:25:08 -0700

    Add files via upload

---
+++ create-table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE cities (
+id INT,
+city VARCHAR(99),
+state_id CHAR(2),
+county VARCHAR(99),
+latitude DECIMAL(7,4),
+longitude DECIMAL(7,4),
+population INT,
+incorporated CHAR(5),
+timezone VARCHAR(99)
+);
